68455bb730a8138eb220b2564745a4a2f08692b1
Jonathan Sanders <jps458@nyu.edu>
2017-07-08 19:24:10 +0000

    ready to deploy

---
+++ mac/macbeth.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as ET
+
+def speeches(act, values):
+    speech = act.findall('SCENE/SPEECH')
+    for x in speech:
+        speakers = x.findall('SPEAKER')
+        lines = x.findall('LINE')
+        for s in speakers:
+            if s.text == 'ALL':
+                pass
+            elif s.text.title() in values:
+                values[s.text.title()] += len(lines)
+            else:
+                values[s.text.title()] = len(lines)
+    return values
+
+def acts(tree):
+    values = {}
+    for child in tree:
+        if child.tag == "ACT": 
+            speeches(child, values)
+    return values
+
+
+#acts(tree.getroot())
+#print(values)
